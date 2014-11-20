9c492146d39584ce162f648bf31c1753ecb8040e
gl <singerinsky>
2014-11-20 17:59:08 +0800

    add new code

--- src/MainFrame.py
+++ src/MainFrame.py
@@ -10,8 +10,12 @@ ID_EDITOR    = 1002
 ID_HELP      = 1003
 
 ID_MENU_ITEM_SOURCE = 2001
+ID_MENU_ADD_MAP= 2002
 
 ID_SOURCE_GRID      = 3001
+
+ID_MAP_WIDTH = 4001
+
 
 class MainFrame(wx.Frame):
 
@@ -25,7 +29,9 @@ class MainFrame(wx.Frame):
         self.menu_bar = wx.MenuBar()
         menu_bar_file_ = wx.Menu()
         menu_bar_file_.Append(ID_MENU_ITEM_SOURCE,u"导入素材文件夹",u"导入素材文件夹")
+        menu_bar_file_.Append(ID_MENU_ADD_MAP,u"add map",u"add map")
         wx.EVT_MENU(self, ID_MENU_ITEM_SOURCE, self.dispatch_event)  
+        wx.EVT_MENU(self, ID_MENU_ADD_MAP, self.dispatch_event)  
         #menu_bar_file_.Append(ID_EDITOR,u"编辑",u"编辑")
         #menu_bar_file_.Append(ID_HELP,u"帮助",u"帮助")
         
@@ -42,6 +48,9 @@ class MainFrame(wx.Frame):
         if event.Id == ID_MENU_ITEM_SOURCE :
             print "open the source folder"
             self.open_file_select()
+        elif event.Id == ID_MENU_ADD_MAP:
+            print "open a new map"
+            self.open_new_map()
         else:
             print "unkown event!"
         
@@ -55,12 +64,12 @@ class MainFrame(wx.Frame):
         file_select.Destroy()
         #pos_ = wx.Point(1,1)
         self.map_grid_ = wx.grid.Grid(self,ID_SOURCE_GRID,size=(300,1000))
-        table_base = wx.grid.GridStringTable(0,2)
+        table_base = wx.grid.GridStringTable(0,1)
         table_base.SetColLabelValue(0,u'文件名')
         #table_base.AppendRows()
         index = 0
         for key ,value in all_files.items():
-            print key
+            print key,value
             table_base.AppendRows(1)
             table_base.SetValue(index,0,key)
             index = index + 1
@@ -71,9 +80,19 @@ class MainFrame(wx.Frame):
         #self.map_grid_.AutoSize()
         #self.map_grid_.Show(False)
         
-        
-        
-        
-        
-        
-        
+    def open_new_map(self):
+        panel = wx.Panel(self,-1)        
+        self.col_txt = wx.TextCtrl(panel,ID_MAP_WIDTH,"rows",size=(175,-1))
+        #self.MoveXY(50,50)
+        #self.col_txt.Show()
+        self.col_txt.SetInsertionPoint(0)
+        sizer=wx.FlexGridSizer(cols=2,hgap=6,vgap=6)
+        sizer.AddMany([self.col_txt])
+        panel.SetSizer(sizer)
+        #self.col_txt.Show()
+        #/self.Bind(wx.EVT_TEXT,,self.col_txt)
+
+
+
+
+
--- src/main.py
+++ src/main.py
@@ -4,7 +4,9 @@ from MainFrame import *
     
 
 if __name__ == '__main__':
+    print "start appliaction"
     app = wx.App(False)
     main_frame = MainFrame(None,"map editor")
+    main_frame.Show()
     app.MainLoop()
     pass
